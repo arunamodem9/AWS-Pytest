e50b2c163983e96e60b8ab9e17ff642cd87adedf
arunamodem9 <110841868+arunamodem9@users.noreply.github.com>
2022-12-22 10:40:15 +0530

    Update ops.py
    failed testing scenario

--- ops.py
+++ ops.py
@@ -1,11 +1,11 @@
 def add(x,y):
-    return x+y
+    return x+y+2
 
 def subtract(x,y):
-    return x-y
+    return x-y-1
 
 def multiply(x,y):
-    return x*y
+    return x*y-5
 
 def divide(x,y):
-    return x/y
+    return x/y+1
